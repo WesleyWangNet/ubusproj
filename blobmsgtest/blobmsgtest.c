ec0a06c352b2be71395795bba99ec570786501f5
ioopsgithub@outlook.com <WelsyWang>
2025-01-18 23:11:19 +0800

    add blobmsg demo

--- blobmsgtest/blobmsgtest.c
+++ blobmsgtest/blobmsgtest.c
@@ -18,21 +18,33 @@ int main(int argc, char **argv)
 
     printf("buf.buflen: %d\r\n", prev_len);
 
+    blobmsg_add_string(&buf, "name", "Openwrt");
+    blobmsg_add_u32(&buf, "id", 100);
 
-#if 0
-    for ( int i = 0; i < BUFF_CHUNKS; i++) {
-        struct blob_attr *attr = blob_new(&buf, 0, BUFF_SIZE);
-        if (!attr) {
-            printf("blob new error\r\n");
-            break;
-        }
-        if (prev_len < buf.buflen) {
-            prev_len = buf.buflen;
-            continue;
-        }
-    }
+    //add object
+    void *tab1 = blobmsg_open_table(&buf, "tab1");
+    blobmsg_add_string(&buf, "tab1_name", "tab1");
+    blobmsg_add_u32(&buf, "tab1_id", 101);
+    blobmsg_close_table(&buf, tab1);
 
-#endif
+    void *tab2 = blobmsg_open_table(&buf, "tab2");
+    blobmsg_add_string(&buf, "tab2_name", "tab2");
+    blobmsg_add_u32(&buf, "tab2_id", 102);
+    blobmsg_close_table(&buf, tab2);
 
+
+    // test hello example
+   // blobmsg_add_blob(&buf1, attr);
+
+    char *jsonstr1 = blobmsg_format_json_value(buf1.head, true);
+    printf("%s\r\n", jsonstr1);
+    char *jsonstr = blobmsg_format_json(buf.head, true);
+    printf("%s\r\n", jsonstr);
+
+    free(jsonstr);
+    free(jsonstr1);
+    blob_buf_free(&buf1);
+    blob_buf_free(&buf);
+    
     return 0;
 }
